8d76a541ae3740037b0ef25b3e00b854d95600d5
JFreyra <efreyra@stuy.edu>
2016-09-22 15:32:35 -0400

    added test.c simpleFunc.c

---
+++ C/simpleFunc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void func(){
+  printf("Hello World!\n");
+}
+
+void main(){
+  func();
+}
---
+++ C/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+  int i = 25;
+
+  printf("size of i: %lu \n",sizeof(i));
+  printf("value of i: %d \n",i);
+  printf("address of i: %lu \n",&i);
+
+}
